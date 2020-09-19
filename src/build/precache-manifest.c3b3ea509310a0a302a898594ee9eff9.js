b688441b8fbea0cac8e4ae91b3e002aadd21d989
feroz <feroz@DESKTOP-ATPPT6R>
2020-09-19 16:30:49 +0530

    First Commit

---
+++ src/build/precache-manifest.c3b3ea509310a0a302a898594ee9eff9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dea023f7a267d5dd79444e59a68fd736",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ec96cf7dad0af7c506d",
+    "url": "/static/css/main.ef4dbe8f.chunk.css"
+  },
+  {
+    "revision": "42dcac51a0a1225718b8",
+    "url": "/static/js/2.50dc2fc4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.50dc2fc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ec96cf7dad0af7c506d",
+    "url": "/static/js/main.255d11d9.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
